bfeb607c69b725e356584cfae9d167ba3177da8a
theHacker <git@thehacker.biz>
2024-06-12 20:21:24 +0200

    Little cleanup - Removed VS Code stuff - Removed CommonJS syntax, we only use ES2015 syntax - Removed TS, JSX, TSX configuration, we don't use that - Cleaned .gitignore - Let IntelliJ reformat everything (indentation 4, use semicolons) - We don't want "lint" script to fix. Added separate "lint:fix" for that. - Put package-lock.json into VCS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 /* eslint-env node */
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+    root: true,
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended'
+    ],
+    overrides: [
+        {
+            files: [
+                'cypress/e2e/**/*.{cy,spec}.js',
+                'cypress/support/**/*.js'
+            ],
+            'extends': [
+                'plugin:cypress/recommended'
+            ]
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
     }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+};
