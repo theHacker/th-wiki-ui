1741f40969c84a10f253c30ccb853483d817da31
theHacker <git@thehacker.biz>
2024-06-17 15:55:10 +0200

    Fix: vitest.config.js can cope with functional vite.config.js again

--- vitest.config.js
+++ vitest.config.js
@@ -2,8 +2,10 @@ import {fileURLToPath} from 'node:url';
 import {mergeConfig, defineConfig, configDefaults} from 'vitest/config';
 import viteConfig from './vite.config';
 
-export default mergeConfig(
-    viteConfig,
+// Use defineConfig() to cope with callback viteConfig.
+// See https://github.com/vitest-dev/vitest/issues/4687#issuecomment-1843181549
+export default defineConfig(config => mergeConfig(
+    viteConfig(config),
     defineConfig({
         test: {
             environment: 'jsdom',
@@ -11,4 +13,4 @@ export default mergeConfig(
             root: fileURLToPath(new URL('./', import.meta.url))
         }
     })
-);
+));
