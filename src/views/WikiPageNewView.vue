6da32969112d45ef9669901e7dcd603911634861
theHacker <git@thehacker.biz>
2024-06-19 12:09:14 +0200

    Refactoring: Extracted <WikiPageEditForm>

--- src/views/WikiPageNewView.vue
+++ src/views/WikiPageNewView.vue
@@ -3,58 +3,15 @@
         <div class="column is-8 is-offset-2">
             <h1 class="title">New wiki page</h1>
 
-            <ErrorMessage v-if="error">{{ error }}</ErrorMessage>
-
-            <fieldset :disabled="saving">
-                <div class="field">
-                    <label class="label">Title</label>
-                    <div class="control">
-                        <input
-                            v-model="entry.title"
-                            class="input"
-                            :class="{'is-danger': !!fieldErrors.title}"
-                            type="text"
-                            placeholder="Title"
-                        />
-                    </div>
-                    <p v-if="fieldErrors.title" class="help is-danger">{{ fieldErrors.title }}</p>
-                </div>
-
-                <div class="field">
-                    <label class="label">Parent</label>
-                    <div class="control">
-                        <WikiPagesSelect
-                            v-model="entry.parentId"
-                            :error="!!fieldErrors.parentId"
-                        />
-                    </div>
-                    <p v-if="fieldErrors.parentId" class="help is-danger">{{ fieldErrors.parentId }}</p>
-                </div>
-
-                <div class="field">
-                    <label class="label">Content</label>
-                    <div class="control">
-                        <textarea
-                            v-model="entry.content"
-                            class="textarea"
-                            :class="{'is-danger': !!fieldErrors.content}"
-                            rows="15"
-                            placeholder="Content"
-                        />
-                    </div>
-                    <p v-if="fieldErrors.content" class="help is-danger">{{ fieldErrors.content }}</p>
-                    <p class="help">Content will be parsed as Markdown.</p>
-                </div>
-
-                <div class="field is-grouped">
-                    <div class="control">
-                        <Button icon="check" title="Create" color="link" :loading="saving" @click="save" />
-                    </div>
-                    <div class="control">
-                        <Button icon="xmark" title="Cancel" color="link" :light="true" @click="cancel" />
-                    </div>
-                </div>
-            </fieldset>
+            <WikiPageEditForm
+                v-model="entry"
+                submitLabel="Create"
+                :saving="saving"
+                :error="error"
+                :fieldErrors="fieldErrors"
+                @submit="save"
+                @cancel="cancel"
+            />
         </div>
     </div>
 </template>
@@ -63,9 +20,7 @@
 import axios from "@/axios.js";
 import {ref} from 'vue';
 import {useRouter} from "vue-router";
-import Button from "@/components/Button.vue";
-import ErrorMessage from "@/components/ErrorMessage.vue";
-import WikiPagesSelect from "@/components/WikiPagesSelect.vue";
+import WikiPageEditForm from "@/components/WikiPageEditForm.vue";
 
 const router = useRouter();
 
