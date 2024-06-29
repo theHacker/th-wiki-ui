97a09329b6abcd844db50bc7231407cef8d7fccf
theHacker <git@thehacker.biz>
2024-06-29 18:52:55 +0200

    Layout for Tasks view is now filled by actual (still demo) data

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -29,28 +29,35 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="i in 15" :class="{'has-text-danger': i === 3 }">
-                        <td><input type="checkbox" disabled :checked="i % 7 === 0" /></td>
+                    <tr v-for="entry in entries" :class="{'has-text-danger': entry.overdue }">
+                        <td><input type="checkbox" disabled :checked="entry.done" /></td>
                         <td
-                            :style="{ paddingLeft: `${[3, 8, 10, 13].includes(i) ? 24 : [4, 5, 14].includes(i) ? 48 : 0}px` }"
+                            :style="{ paddingLeft: `${entry.indentLevel * 24}px` }"
                         >
                             <div class="level">
                                 <div class="level-left">
-                                    <div class="level-item" :style="(i % 7 === 0) ? 'text-decoration: var(--bulma-grey) 2px solid line-through' : ''">
-                                        Demo TODO {{ i }} something to do
+                                    <div
+                                        class="level-item"
+                                        :style="entry.done ? 'text-decoration: var(--bulma-grey) 2px solid line-through' : ''"
+                                    >
+                                        {{ entry.title }}
                                     </div>
                                 </div>
                                 <div class="level-right">
-                                    <div v-if="i % 4 === 0" class="level-item">
-                                        <progress class="progress is-primary" value="25" max="100">25%</progress>
+                                    <div v-if="!entry.done && entry.progress > 0" class="level-item">
+                                        <progress
+                                            class="progress is-primary"
+                                            :value="entry.progress"
+                                            max="100"
+                                        >{{ entry.progress }}%</progress>
                                     </div>
                                 </div>
                             </div>
                         </td>
-                        <td>2024-05-04</td>
+                        <td>{{ entry.createDate }}</td>
                         <td>
-                            <span :class="{'is-italic': i % 3 !== 0, 'has-text-weight-semibold': i === 3}">
-                                {{ i === 3 ? '2024-01-01' : i % 3 === 0 ? '2024-05-04' : 'no due date' }}
+                            <span :class="{'is-italic': !entry.dueDate, 'has-text-weight-semibold': entry.overdue }">
+                                {{ entry.dueDate || 'no due date' }}
                             </span>
                         </td>
                         <td>
@@ -61,7 +68,7 @@
                                         tooltip="Move up"
                                         size="small"
                                         color="info"
-                                        :disabled="[1, 3, 4, 8, 10, 13, 14].includes(i)"
+                                        :disabled="!entry.canBeMovedUp"
                                     />
                                 </div>
                                 <div class="control">
@@ -70,15 +77,15 @@
                                         tooltip="Move down"
                                         size="small"
                                         color="info"
-                                        :disabled="[3, 5, 8, 10, 13, 14, 15].includes(i)"
+                                        :disabled="!entry.canBeMovedDown"
                                     />
                                 </div>
                                 <div class="control">
                                     <Button
-                                        :icon="i % 7 === 0 ? 'xmark' : 'check'"
-                                        :tooltip="i % 7 === 0 ? 'Mark undone' : 'Mark down'"
+                                        :icon="entry.done ? 'xmark' : 'check'"
+                                        :tooltip="entry.done ? 'Mark undone' : 'Mark down'"
                                         size="small"
-                                        :color="i % 7 === 0 ? 'warning' : 'success'"
+                                        :color="entry.done ? 'warning' : 'success'"
                                     />
                                 </div>
                                 <div class="control">
@@ -120,6 +127,29 @@ import SearchInput from "@/components/SearchInput.vue";
 import {ref} from "vue";
 
 const search = ref('');
+
+// Demo data only
+const entries = Array(15)
+    .fill(undefined)
+    .map((_, index) => {
+        const i = index + 1;
+
+        const title = `Demo TODO ${i} something to do`;
+        const done = (i % 7 === 0);
+        const progress = done ? 100 : (i % 4 === 0) ? 25 : 0;
+        const createDate = (i % 2 === 0) ? '2024-05-04' : '2024-05-02';
+        const dueDate = (i === 3) ? '2024-01-01' : (i % 3 === 0) ? '2024-05-04' : null;
+
+        // TODO these will be calculated eventually
+        const overdue = (i === 3);
+        const indentLevel = [3, 8, 10, 13].includes(i) ? 1 : [4, 5, 14].includes(i) ? 2 : 0;
+        const canBeMovedUp = ![1, 3, 4, 8, 10, 13, 14].includes(i);
+        const canBeMovedDown = ![3, 5, 8, 10, 13, 14, 15].includes(i);
+
+        return {
+            title, done, progress, createDate, dueDate, overdue, indentLevel, canBeMovedUp, canBeMovedDown
+        };
+    });
 </script>
 
 <style lang="scss" scoped>
