424481ffa5570af936f3b87fd4f1d067decfbcca
theHacker <git@thehacker.biz>
2024-07-28 10:23:02 +0200

    Git version information - Git hash and dirty flag is displayed in the footer - Add git-describe library - CI "build" step installs Git to be able to get version information

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,15 @@
     </main>
 
     <footer class="py-3 is-size-7 has-text-centered has-background-dark">
-        powered by <b>tH-Wiki</b>
+        powered by <b>tH-Wiki</b><br />
+        Git commit <i>{{ gitInfo }}</i> <b v-if="gitDirty">(dirty)</b>
     </footer>
 </template>
 
 <script setup>
 import {RouterView} from 'vue-router';
 import TheMainNavigation from "@/components/general/TheMainNavigation.vue";
+
+const gitInfo = import.meta.env.VITE_GIT_HASH;
+const gitDirty = import.meta.env.VITE_GIT_DIRTY === 'true';
 </script>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import {fileURLToPath, URL} from 'node:url';
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import VueDevTools from 'vite-plugin-vue-devtools';
+import GitDescribe from "git-describe";
+
+try {
+    const gitInfo = GitDescribe.gitDescribeSync({
+        customArguments: ['--abbrev=40']
+    });
+    process.env.VITE_GIT_HASH = gitInfo.hash;
+    process.env.VITE_GIT_DIRTY = gitInfo.dirty.toString();
+} catch (e) {
+    // ignore, we end up here when running tests on CI (error is "Git executable not found in PATH")
+    process.env.VITE_GIT_HASH = '';
+    process.env.VITE_GIT_DIRTY = '';
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
