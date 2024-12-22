767efe460c43ae2977169048358aa201d4d7dc5d
theHacker <git@thehacker.biz>
2024-12-23 00:10:18 +0100

    <MarkdownTextarea> now provides a preview functionality

--- src/components/general/MarkdownTextarea.vue
+++ src/components/general/MarkdownTextarea.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="position-relative">
         <label class="form-label">{{ label }}</label>
-        <Button
-            class="position-absolute top-0 end-0"
-            icon="code"
-            tooltip="Switch to monospace font"
-            size="small"
-            :active="monospaceFont"
-            @click="monospaceFont = !monospaceFont"
-        />
+        <div class="position-absolute top-0 end-0 hstack gap-1">
+            <Button
+                icon="code"
+                tooltip="Switch to monospace font"
+                size="small"
+                fixedWidth
+                :active="monospaceFont"
+                @click="monospaceFont = !monospaceFont"
+            />
+            <Button
+                icon="newspaper"
+                tooltip="Show preview"
+                size="small"
+                fixedWidth
+                :active="showPreview"
+                @click="showPreview = !showPreview"
+            />
+        </div>
         <textarea
             v-model="model"
             class="form-control"
@@ -18,12 +28,23 @@
         />
         <div v-if="errorMessage" class="invalid-feedback">{{ errorMessage }}</div>
         <div class="form-text">Content will be parsed as Markdown.</div>
+
+        <div v-if="showPreview" class="card mt-3">
+            <div class="card-header text-bg-info">
+                <i class="fas fa-newspaper pe-1" />
+                Preview
+            </div>
+            <div class="card-body">
+                <article v-html="preview" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import Button from "@/components/Button.vue";
+import {renderMarkdown} from "@/markdown.js";
 
 const model = defineModel();
 
@@ -43,4 +64,13 @@ defineProps({
 });
 
 const monospaceFont = ref(false);
+
+const showPreview = ref(false);
+const preview = computed(() => {
+    if (showPreview.value) {
+        return renderMarkdown(model.value);
+    } else {
+        return null; // don't render on every change when preview is not visible
+    }
+});
 </script>
