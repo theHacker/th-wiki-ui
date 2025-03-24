c108590b9126518ff6523bea729b4ec6d886c75c
theHacker <git@thehacker.biz>
2025-03-24 18:56:37 +0100

    THWIKI-82: Foundation for GraphQL API - New environment variable GRAPHQL_API_URL - Axios instance provides a graphql() helper function, taking care of request method and headers

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
 
 const apiUrl = window.env.API_URL || import.meta.env.VITE_API_URL;
+const graphqlApiUrl = window.env.GRAPHQL_API_URL || import.meta.env.VITE_GRAPHQL_API_URL;
 
 const instance = axios.create({
     baseURL: apiUrl
 });
+
+instance.graphql = function(query, variables) {
+    return this.post(graphqlApiUrl, { query, variables }, {
+        headers: {
+            // see https://github.com/graphql/graphql-over-http/blob/main/spec/GraphQLOverHTTP.md#media-types,
+            // and https://github.com/graphql/graphql-over-http/blob/main/spec/GraphQLOverHTTP.md#accept
+            'Content-Type': 'application/json',
+            'Accept': 'application/graphql-response+json'
+        }
+    });
+}
 
 export default instance;
