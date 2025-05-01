1b509ac9bb18028858025d396bd6f2bcd763786f
theHacker <git@thehacker.biz>
2025-05-01 16:10:48 +0200

    THWIKI-157: Tags on wiki pages - Tags can be added to/removed from wiki pages. We have a new component <TagsDialog>. It's flexible to be used for issues later, too. - Wiki pages show assigned tags. <BaseHeading> supports showing tags next to the heading.

--- src/components/BaseHeading.vue
+++ src/components/BaseHeading.vue
@@ -1,7 +1,22 @@
 <template>
-    <h1 class="mb-0">
-        <slot />
-    </h1>
+    <div class="d-inline-flex">
+        <h1 class="mb-0">
+            <slot />
+        </h1>
+        <div class="ms-3 hstack align-items-start gap-2">
+            <TagBadge
+                v-for="(tag, index) in tags"
+                :key="index"
+                :scope="tag.scope"
+                :scopeIcon="tag.scopeIcon"
+                :scopeColor="tag.scopeColor"
+                :title="tag.title"
+                :titleIcon="tag.titleIcon"
+                :titleColor="tag.titleColor"
+                :tooltip="tag.description"
+            />
+        </div>
+    </div>
     <div class="mb-heading hstack gap-2">
         <div v-if="smallText">
             <small>{{ smallText }}</small>
@@ -10,9 +25,14 @@
 </template>
 
 <script setup>
+import TagBadge from "@/components/TagBadge.vue";
+
 defineProps({
     smallText: {
         type: String
+    },
+    tags: {
+        type: Array
     }
 });
 </script>
