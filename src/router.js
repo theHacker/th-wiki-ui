80853131361b011a31c0bb95f3fb0cff44513acf
theHacker <git@thehacker.biz>
2025-05-02 20:07:36 +0200

    THWIKI-112: Removed <DemoView>

--- src/router.js
+++ src/router.js
@@ -47,11 +47,6 @@ const router = createRouter({
             ]
         },
         {
-            path: '/demo',
-            name: 'demo',
-            component: () => import('./views/DemoView.vue')
-        },
-        {
             path: '/issues',
             children: [
                 {
